3d99f9934882c7589aa58313b73f5caffea810dc
Kevin Kim <kkim@Kevins-MacBook-Air.local>
2024-07-29 18:15:43 -0700

    search meteorite by name functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import Map from "./Map";
-import { getMeteoritesByDate } from "./meteorites";
+import { getMeteoriteByName, getMeteoritesByDate } from "./meteorites";
+
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
 const getInitialData = () => {
   const sortedByDate = getMeteoritesByDate().slice(0, 300);
@@ -20,18 +24,50 @@ const getInitialData = () => {
 };
 
 function App() {
-  console.log(getInitialData());
-  const [count, setCount] = useState(0);
-  const [selectedMeteorite, setSelectedMeteorite] = useState({});
+  const [mapData, setMapData] = useState(getInitialData());
+  const [selectedMeteorite, setSelectedMeteorite] = useState({
+    name: "---",
+    year: "---",
+    mass: "---",
+  });
+  const [searchName, setSearchName] = useState("");
 
   const handleOnClick = (data) => {
     setSelectedMeteorite(data);
   };
 
+  const handleSearch = (query) => {
+    setSearchName(query);
+  };
+
+  const handleSearchSubmit = () => {
+    const meteorite = getMeteoriteByName(searchName);
+    setSelectedMeteorite(meteorite);
+    console.log(meteorite);
+  };
+
   return (
     <>
       <h1>Meteorite Madness</h1>
-      <Map data={getInitialData()} onClick={handleOnClick} />
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": { m: 1, width: "25ch" },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          id="outlined-basic"
+          label="Search By Name"
+          variant="outlined"
+          onChange={(event) => handleSearch(event.target.value)}
+        />
+        <Button variant="contained" onClick={handleSearchSubmit}>
+          Submit
+        </Button>
+      </Box>
+      <Map data={mapData} onClick={handleOnClick} />
       <div>
         <div>Name: {selectedMeteorite.name}</div>
         <div>Year: {new Date(selectedMeteorite.year).getFullYear()}</div>
--- src/meteorites.ts
+++ src/meteorites.ts
@@ -1,9 +1,52 @@
 import { data } from "./data";
+interface Meteorite {
+  name: string;
+  id: number;
+  nametype: string;
+  recclass: string;
+  mass: string;
+  fall: string;
+  reclat?: string;
+  reclong?: string;
+  geolocation?: {
+    type: string;
+    coordinates: number[];
+  };
+}
 
-// data length = 1000
+const noData = {
+  long: null,
+  lat: null,
+  name: "N/A",
+  id: null,
+  year: null,
+  mass: null,
+};
 
 export const getMeteoritesByDate = () => {
   return data.sort((a, b) => {
     return new Date(b.year) - new Date(a.year);
   });
+};
+
+const searchMeteoriteByName = (arr, query: string) => {
+  console.log(arr);
+  const middleIndex = Math.floor(arr.length / 2);
+  const middleVal = arr[middleIndex];
+  if (middleVal.name === query) return middleVal;
+  if (arr.length <= 1) return false;
+  if (middleVal.name < query) {
+    return searchMeteoriteByName(arr.slice(middleIndex), query);
+  } else if (middleVal.name > query) {
+    return searchMeteoriteByName(arr.slice(0, middleIndex), query);
+  }
+  return false;
+};
+
+export const getMeteoriteByName = (query: string) => {
+  const sortByName = data.sort((a, b) => {
+    return a.name.localeCompare(b.name);
+  });
+  const match = searchMeteoriteByName(sortByName, query);
+  return match ? match : noData;
 };
